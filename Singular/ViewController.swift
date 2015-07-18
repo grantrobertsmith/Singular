dd7993ca9072a44bfeb40407ba1dcbc4b21315bc
Grant Smith <grantrobertsmith@gmail.com>
2015-07-17 23:10:55 -0700

    Initial Commit

---
+++ Singular/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Singular
+//
+//  Created by Grant Smith on 7/17/15.
+//  Copyright (c) 2015 InfoSync. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
